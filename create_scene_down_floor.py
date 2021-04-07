f64b01929de7428233d053c99e4e9c282f47dcee
VitoDebellis <vito.debellis.1993@gmail.com>
2021-04-07 10:56:07 +0200

    aggiunta nuova stanza

---
+++ create_scene_down_floor.py
@@ -0,0 +1,103 @@
+import arcade
+
+WALL_RESOURCE = "resources/muro.png"
+
+
+def create_collidable(screen_width, screen_height):
+    walls = arcade.SpriteList(
+        use_spatial_hash=True, spatial_hash_cell_size=128)
+
+    walls.extend(create_outer_walls(screen_width, screen_height))
+    
+    return walls
+
+
+def create_not_collidable():
+    texture = arcade.SpriteList(is_static=True)
+
+    texture.extend(create_stair())
+    texture.extend(create_lift())
+
+    return texture
+
+# Posizionamento dei muri
+
+
+def create_outer_walls(screen_width, screen_height):
+    print("down floor")
+    SPRITE_SIZE = 16
+    wall_list = arcade.SpriteList(
+        use_spatial_hash=True, spatial_hash_cell_size=128)
+
+    # muro superiore
+    for i in range(int(screen_width/SPRITE_SIZE)):
+        sprite = arcade.Sprite(
+            WALL_RESOURCE)
+
+        x = SPRITE_SIZE*i+8
+        y = screen_height-8
+
+        sprite.center_x = x
+        sprite.center_y = y
+        wall_list.append(sprite)
+
+    # muro inferiore
+    for i in range(int(screen_width/SPRITE_SIZE)):
+        sprite = arcade.Sprite(
+            WALL_RESOURCE)
+
+        x = SPRITE_SIZE*i+8
+        y = 8
+
+        sprite.center_x = x
+        sprite.center_y = y
+        wall_list.append(sprite)
+
+    # muro sinistro
+    for i in range(1, int(screen_height/SPRITE_SIZE)-1):
+        sprite = arcade.Sprite(
+            WALL_RESOURCE)
+
+        x = 8
+        y = SPRITE_SIZE*i+8
+
+        sprite.center_x = x
+        sprite.center_y = y
+        wall_list.append(sprite)
+
+    # muro destro
+    for i in range(1, int(screen_height/SPRITE_SIZE)-1):
+        sprite = arcade.Sprite(
+            WALL_RESOURCE)
+
+        x = screen_width-8
+        y = SPRITE_SIZE*i+8
+
+        sprite.center_x = x
+        sprite.center_y = y
+        wall_list.append(sprite)
+
+    return wall_list
+
+def create_stair():
+    texture_list = arcade.SpriteList(is_static=True)
+    texture = arcade.Sprite(
+        "resources/stairs.png", 0.9)
+    texture.angle = 90
+    texture.center_x = 744
+    texture.center_y = 504
+    texture_list.append(texture)
+
+    return texture_list
+
+
+def create_lift():
+    texture_list = arcade.SpriteList(is_static=True)
+    texture = arcade.Sprite(
+        "resources/lift.png", 0.97)
+    texture.center_x = 320
+    texture.center_y = 54
+    texture_list.append(texture)
+    
+
+    return texture_list
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import arcade
-import create_scene
-import create_scene2
+import create_scene_ground_floor
+import create_scene_up_floor
+import create_scene_down_floor
 from graph import Graph
 from path_finding import A_star
 
@@ -27,15 +28,16 @@ class MyGame(arcade.Window):
         self.barrier_list = None
         self.graph = None
         self.timer = 0
+        self.timer_scene = 2
         
         arcade.set_background_color(arcade.color.WHITE)
 
-    def setup(self):
+    def setup_ground_floor(self):
         self.check = 1
         self.player_list = arcade.SpriteList()
-        self.wall_list = create_scene.create_collidable(
+        self.wall_list = create_scene_ground_floor.create_collidable(
             SCREEN_WIDTH, SCREEN_HEIGHT)
-        self.texture_list = create_scene.create_not_collidable()
+        self.texture_list = create_scene_ground_floor.create_not_collidable()
         self.player = arcade.Sprite(
             "resources/robottino.png")
         self.player.center_x = SPRITE_SIZE * 5
@@ -46,19 +48,35 @@ class MyGame(arcade.Window):
         self.physics_engine = arcade.PhysicsEngineSimple(self.player,
                                                          self.wall_list)
     
-    def setup2(self):
-        
+    def setup_up_floor(self):
+        self.check = 2
         self.player_list = arcade.SpriteList()
-        self.wall_list = create_scene2.create_collidable(
+        self.wall_list = create_scene_up_floor.create_collidable(
             SCREEN_WIDTH, SCREEN_HEIGHT)
-        self.texture_list = create_scene2.create_not_collidable()
+        self.texture_list = create_scene_up_floor.create_not_collidable()
         self.player = arcade.Sprite(
             "resources/robottino.png")
         self.player.center_x = SPRITE_SIZE * 5
         self.player.center_y = SPRITE_SIZE * 1
         self.player_list.append(self.player)
         self.graph = Graph(self.wall_list)
-        self.check = 2
+        
+
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player,
+                                                         self.wall_list)
+    
+    def setup_down_floor(self):
+        self.check = 3
+        self.player_list = arcade.SpriteList()
+        self.wall_list = create_scene_down_floor.create_collidable(
+            SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.texture_list = create_scene_down_floor.create_not_collidable()
+        self.player = arcade.Sprite(
+            "resources/robottino.png")
+        self.player.center_x = SPRITE_SIZE * 5
+        self.player.center_y = SPRITE_SIZE * 1
+        self.player_list.append(self.player)
+        self.graph = Graph(self.wall_list)
 
         self.physics_engine = arcade.PhysicsEngineSimple(self.player,
                                                          self.wall_list)
@@ -74,8 +92,11 @@ class MyGame(arcade.Window):
         self.physics_engine.update()
         #fare controllo di posizione e scena, se scena 1 e posizione in ascensore o scale, richiama scena 2
         #[0] = x, [1] = y;
-        if (self.player.position[0] > 271.5 and self.player.position[0] < 368.5) and (self.player.position[1] > 15.2 and self.player.position[1] < 92.8):
-            self.cambia()
+        if self.timer_scene == 2:
+            if (self.player.position[0] > 271.5 and self.player.position[0] < 368.5) and (self.player.position[1] > 15.2 and self.player.position[1] < 92.8) or (self.player.position[0] > 703.5 and self.player.position[0] < 784.5) and (self.player.position[1] > 463.5 and self.player.position[1] < 544.5):
+                self.timer_scene = 0
+                self.cambia()
+        
         
             
 
@@ -90,23 +111,25 @@ class MyGame(arcade.Window):
         robot_pos = self.player.position
         self.path = A_star(self.graph, [robot_pos], (x, y))
         print(x,y)
+        self.timer_scene = 2
         
     
     def cambia(self):
         if self.check == 1:
             self.clear()
-            self.setup2()
-            self.player.position= (320,100)
+            self.setup_up_floor()            
         else:
             self.clear()
-            self.setup()
-            self.player.position= (320,100)
+            self.setup_ground_floor()
+            
+            
+        
         
     
 def main():
     """ Main method """
     window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    window.setup2()
+    window.setup_ground_floor()
     arcade.run()
 
 
