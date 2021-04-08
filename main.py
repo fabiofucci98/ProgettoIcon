fc24d8e6d004355390f4a809be69820603fbf251
VitoDebellis <vito.debellis.1993@gmail.com>
2021-04-08 10:58:15 +0200

    aggiunta animazione di uscita ascensore/scale, ridimensionata schermata

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from path_finding import A_star
 
 
 SPRITE_SIZE = 32
-SCREEN_WIDTH = 1024
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-SCREEN_WIDTH_ROOM = 800
+
 
 SCREEN_TITLE = "Culo"
 
@@ -37,7 +37,7 @@ class MyGame(arcade.Window):
         self.check = 1
         self.player_list = arcade.SpriteList()
         self.wall_list = create_scene_ground_floor.create_collidable(
-            SCREEN_WIDTH_ROOM, SCREEN_HEIGHT)
+            SCREEN_WIDTH, SCREEN_HEIGHT)
         self.texture_list = create_scene_ground_floor.create_not_collidable()
         self.player = arcade.Sprite(
             "resources/robottino.png")
@@ -53,7 +53,7 @@ class MyGame(arcade.Window):
         self.check = 2
         self.player_list = arcade.SpriteList()
         self.wall_list = create_scene_up_floor.create_collidable(
-            SCREEN_WIDTH_ROOM, SCREEN_HEIGHT)
+            SCREEN_WIDTH, SCREEN_HEIGHT)
         self.texture_list = create_scene_up_floor.create_not_collidable()
         self.player = arcade.Sprite(
             "resources/robottino.png")
@@ -69,7 +69,7 @@ class MyGame(arcade.Window):
         self.check = 3
         self.player_list = arcade.SpriteList()
         self.wall_list = create_scene_down_floor.create_collidable(
-            SCREEN_WIDTH_ROOM, SCREEN_HEIGHT)
+            SCREEN_WIDTH, SCREEN_HEIGHT)
         self.texture_list = create_scene_down_floor.create_not_collidable()
         self.player = arcade.Sprite(
             "resources/robottino.png")
@@ -80,13 +80,13 @@ class MyGame(arcade.Window):
         self.physics_engine = arcade.PhysicsEngineSimple(self.player,
                                                          self.wall_list)
 
-    def in_stairs(self):
-        if (self.player.position[0] > 271.5 and self.player.position[0] < 368.5) and (self.player.position[1] > 15.2 and self.player.position[1] < 92.8):
+    def in_elevator(self): #271.5 - 368.5 / 15.2 - 92.8
+        if (self.player.position[0] > 303.5 and self.player.position[0] < 336.5) and (self.player.position[1] > 37.2 and self.player.position[1] < 64.1):
             return True
         return False
 
-    def in_elevator(self):
-        if (self.player.position[0] > 703.5 and self.player.position[0] < 784.5) and (self.player.position[1] > 463.5 and self.player.position[1] < 544.5):
+    def in_stairs(self): #703.5 - 784.5 / 463.5 - 544.5
+        if (self.player.position[0] > 719.5 and self.player.position[0] < 768.5) and (self.player.position[1] > 479.5 and self.player.position[1] < 528.5):
             return True
         return False
 
@@ -106,9 +106,15 @@ class MyGame(arcade.Window):
         # fare controllo di posizione e scena, se scena 1 e posizione in ascensore o scale, richiama scena 2
         # [0] = x, [1] = y;
         self.timer_scene += delta_time
-        if self.in_stairs() or self.in_elevator():
+        if self.in_elevator():
             if self.timer_scene > 2:
                 self.timer_scene = 0
+                self.path = A_star(self.graph, [self.player.position], (320, 112))
+                self.cambia(self.player.position)
+        elif self.in_stairs():
+            if self.timer_scene > 2:
+                self.timer_scene= 0
+                self.path = A_star(self.graph, [self.player.position], (688,512))
                 self.cambia(self.player.position)
 
     def on_draw(self):
@@ -118,16 +124,21 @@ class MyGame(arcade.Window):
         self.player_list.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        if x < 800:
-            x, y = x-x % 16, y-y % 16
-            robot_pos = self.player.position
-            self.path = A_star(self.graph, [robot_pos], (x, y))
+        x, y = x-x % 16, y-y % 16
+        robot_pos = self.player.position
+        self.path = A_star(self.graph, [robot_pos], (x, y))
+        print ("partenza",robot_pos)
+        print("arrivo",x,y)
+
 
     def cambia(self, robot_pos):
         if self.check == 1:
             self.clear()
             self.setup_up_floor(robot_pos)
-        else:
+        elif self.check == 2:
+            self.clear()
+            self.setup_down_floor(robot_pos)
+        elif self.check == 3:
             self.clear()
             self.setup_ground_floor(robot_pos)
 
